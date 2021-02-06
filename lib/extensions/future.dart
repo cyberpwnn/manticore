6b748447419150b084942f35a78980f8490f4d16
Daniel Mills <danielmillst@gmail.com>
2021-02-06 05:24:17 -0500

    Future extensions

---
+++ lib/extensions/future.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+
+typedef bool Predicate<X>(X x);
+typedef Widget WBuilder<T>(BuildContext context, T t);
+typedef Widget LBuilder(BuildContext context);
+typedef X Callback<X>();
+
+extension FutureExtension<T> on Future<T> {
+  /// Will also tack on the given future to the existing (instance) future.
+  /// The resulting future will only complete once both futures have completed
+  Future<V> and<V>(Future<V> v) async =>
+      Future.wait(<Future>[this, v]).then((r) => r[1]);
+
+  /// If the given predicate is true (checked when the future completes)
+  /// Invoke the callback result, otherwise returns null
+  Future<G> predicate<G>(Predicate<T> predicate, Callback<G> callback) =>
+      this.then((f) {
+        if (predicate(f)) {
+          return callback();
+        }
+
+        return null;
+      });
+
+  /// Returns a futurebuilder out of this future
+  /// Provide a builder(data, context) and a loading(context) builder
+  FutureBuilder<T> build({
+    WBuilder builder,
+    LBuilder loading,
+  }) =>
+      FutureBuilder(
+        builder: (context, snap) {
+          if (!snap.hasData) {
+            return loading != null ? loading(context) : Container();
+          }
+
+          return builder != null ? builder(context, snap.data) : Container();
+        },
+        future: this,
+      );
+}
